6b925ab2c3523128c4bf29a5bf8ee2154532746a
heyjaisal <heyjaisal@gmail.com>
2026-03-13 16:57:34 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.NODE_ENV === "production" ? "export" : undefined,
+  output: "export",
+  trailingSlash: true,
 
   images: {
     unoptimized: true,
