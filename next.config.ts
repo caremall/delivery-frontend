dfacbf377c8b6cc4300a840160c3003adec3bbd6
heyjaisal <heyjaisal@gmail.com>
2026-03-13 15:15:52 +0530

    upate

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",   // 👈 REQUIRED for S3 static hosting
+
   images: {
+    unoptimized: true,   // 👈 required when using static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'caremall-web.s3.ap-south-1.amazonaws.com',
+        protocol: "https",
+        hostname: "caremall-web.s3.ap-south-1.amazonaws.com",
       },
     ],
   },
